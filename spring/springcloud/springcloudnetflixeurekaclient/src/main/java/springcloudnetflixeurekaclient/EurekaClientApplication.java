702dc6b743161dff98be43ec125a5dae7987f5c0
abirkhan04 <abir1sumon@gmail.com>
2018-05-12 20:39:38 +0600

    Spring cloud netflix Eureka Client And Server modified.

--- spring/springcloud/springcloudnetflixeurekaclient/src/main/java/springcloudnetflixeurekaclient/EurekaClientApplication.java
+++ spring/springcloud/springcloudnetflixeurekaclient/src/main/java/springcloudnetflixeurekaclient/EurekaClientApplication.java
@@ -7,31 +7,32 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
+@EnableEurekaClient
 @SpringBootApplication
 public class EurekaClientApplication {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        SpringApplication.run(EurekaClientApplication.class);
-       }
+        SpringApplication
+            .run(EurekaClientApplication.class);
     }
+}
 
-    @RestController
-    class ServiceInstanceRestController {
+@RestController
+class ServiceInstanceRestController {
 
-        @Autowired
-        private DiscoveryClient discoveryClient;
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
-        @RequestMapping("/service-instances/{applicationName}")
-        public List<ServiceInstance> serviceInstancesByApplicationName(
-                @PathVariable String applicationName) {
-            return this.discoveryClient.getInstances(applicationName);
-        }
+    @RequestMapping("/services/{applicationname}")
+    public List<ServiceInstance> registeredServicesByName(
+        @PathVariable String applicationname) {
+        return this.discoveryClient
+            .getInstances(applicationname);
     }
+}
