dee6f6f63087676894caf24891bd8569d1896aab
abirkhan04 <abir1sumon@gmail.com>
2018-09-09 21:39:58 +0600

    [Spring-boot-data-jpa] Article project is added.

---
+++ spring/springdata/springbootdatajpa/springbootdatajpa/src/main/java/com/javaworm/springbootdatajpa/SpringbootdatajpaApplication.java
@@ -0,0 +1,12 @@
+package com.javaworm.springbootdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdatajpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootdatajpaApplication.class, args);
+	}
+}
---
+++ spring/springdata/springbootdatajpa/springbootdatajpa/src/test/java/com/javaworm/springbootdatajpa/SpringbootdatajpaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.javaworm.springbootdatajpa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootdatajpaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
