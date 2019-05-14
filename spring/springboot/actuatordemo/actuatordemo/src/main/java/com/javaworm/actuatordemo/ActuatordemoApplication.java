e9b9c7b4ad07216f08369cf410c17524e01eb3ec
abirkhan <abir1sumon@gmail.com>
2019-05-14 23:21:39 +0600

    [springboot] Spring booot actuator demo implementation.

---
+++ spring/springboot/actuatordemo/actuatordemo/src/main/java/com/javaworm/actuatordemo/ActuatordemoApplication.java
@@ -0,0 +1,13 @@
+package com.javaworm.actuatordemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatordemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActuatordemoApplication.class, args);
+	}
+
+}
