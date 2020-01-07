2c15ab94abab2afc84d846af58bbe35cf56c078f
unknown <abir1sumon@gmail.com>
2020-01-07 01:56:47 -0800

    [] Choice router is added on managing exception.

--- mule/exceptionstrategy/src/main/java/com/javaworm/transformer/PayloadTransformer.java
+++ mule/exceptionstrategy/src/main/java/com/javaworm/transformer/PayloadTransformer.java
@@ -4,12 +4,21 @@ import org.mule.api.MuleMessage;
 import org.mule.api.transformer.TransformerException;
 import org.mule.transformer.AbstractMessageTransformer;
 
-public class PayloadTransformer extends AbstractMessageTransformer{
+public class PayloadTransformer extends AbstractMessageTransformer {
 
 	@Override
-	public Object transformMessage(MuleMessage message, String outputEncoding) throws TransformerException {		
-		int i = 0;
-		int b = 3/i;
+	public Object transformMessage(MuleMessage message, String outputEncoding) throws TransformerException {
+		Integer payload = null;
+		try {
+			payload = Integer.valueOf(message.getPayloadAsString());
+			if (payload < 1) {
+				throw new Exception();
+			}
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return message;
 	}
 
