51ef2ddd94b37fafbc7f783446a294543e2f3057
abirkhan04 <abir1sumon@gmail.com>
2020-01-25 13:56:11 +0600

    [] Angular internationalization

---
+++ javascript/internationalization/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule,LOCALE_ID } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [{ provide: LOCALE_ID, useValue: 'fr' }],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
