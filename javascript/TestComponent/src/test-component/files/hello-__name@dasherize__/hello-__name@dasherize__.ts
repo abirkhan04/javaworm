b9fbdac9b9adc8be2220c71dcde598ebf40c9c37
abirkhan <abir1sumon@gmail.com>
2020-03-17 22:15:47 +0600

    [] A Short Discussion about Angular Schematics - Article project ongoing.

---
+++ javascript/TestComponent/src/test-component/files/hello-__name@dasherize__/hello-__name@dasherize__.ts
@@ -0,0 +1,7 @@
+// @Component({
+//     selector: 'hello -<%= dasherize(name) %>'
+// })
+// export class Hello<%= classify(name) %>Component {
+
+// }
+console.log('hello <%= name%>');
--- javascript/TestComponent/src/test-component/index.ts
+++ javascript/TestComponent/src/test-component/index.ts
@@ -1,11 +1,37 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
-
+import { Rule, SchematicContext, Tree, url, apply, template, mergeWith } from '@angular-devkit/schematics';
+import { getWorkspace } from '@schematics/angular/utility/config';
+import { join, normalize } from 'path';
+import { strings } from '@angular-devkit/core';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function testComponent(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    tree.create(_options.name || 'hello', 'world');
-    return tree;
+export function setupOptions(host: Tree, options: any): Tree {
+  const workspace = getWorkspace(host);
+  if (!options.project) {
+    options.project = Object.keys(workspace.projects)[0];
+  }
+  const project = workspace.projects[options.project];
+  options.path = join(normalize(project.root), 'src');
+  return host;
+}
+
+
+export function testComponent(_options: Schema): Rule {
+  return (_tree: Tree, _context: SchematicContext) => {
+    // const { name } = _options;
+    // tree.create('hello.js', `console.log('Hello ${name}!')`);
+    const sourceTemplates = url('./files');
+    const sourceParameterizedTemplates = apply(sourceTemplates, [
+      template({
+        ..._options,
+        ...strings,
+      })
+    ]);
+    return mergeWith(sourceParameterizedTemplates);
   };
+}
+
+export interface Schema {
+  name: string;
+  project?: string;
 }
