2a098fedf857793ce93090d50a4403612275a821
unknown <abir1sumon@gmail.com>
2020-05-06 09:11:55 -0700

    [] Replaced with logger.

--- corejava/JavaGenerics/src/com/javaworm/GenericsExample.java
+++ corejava/JavaGenerics/src/com/javaworm/GenericsExample.java
@@ -1,21 +1,25 @@
 package com.javaworm;
 
 import java.time.LocalDate;
+import java.util.logging.Logger;
 import com.javaworm.entity.FootBall;
 import com.javaworm.generics.GenericDemo;
 import com.javaworm.generics.TestGeneric;
 
 public class GenericsExample {
 
+	private static Logger logger = Logger.getLogger(GenericsExample.class.getCanonicalName());
+
 	public static void main(String args[]) {
 		TestGeneric<Integer, Integer> testGeneric = new TestGeneric<Integer, Integer>(2, 2);
-		System.out.println("Test Generic: " + testGeneric.compareParameters());
+		logger.info("Test Generic: " + testGeneric.compareParameters());
 		FootBall footBall = new FootBall(LocalDate.now(), "Dhaka");
 		footBall.makeGame(footBall);
 
 		GenericDemo<String> genericDemo = new GenericDemo<String>();
 		genericDemo.set("Test");
-		String t =  genericDemo.get();
+		String t = genericDemo.get();
+		logger.info("T: " + t);
 	}
 
 }
