0ed6a1dc14ef9352d328e08e0d2aa6cfe14479af
unknown <abir1sumon@gmail.com>
2020-05-06 08:33:19 -0700

    [] Generic article project enhancement.

--- corejava/JavaGenerics/src/com/javaworm/GenericsExample.java
+++ corejava/JavaGenerics/src/com/javaworm/GenericsExample.java
@@ -2,13 +2,20 @@ package com.javaworm;
 
 import java.time.LocalDate;
 import com.javaworm.entity.FootBall;
+import com.javaworm.generics.GenericDemo;
 import com.javaworm.generics.TestGeneric;
 
 public class GenericsExample {
+
 	public static void main(String args[]) {
 		TestGeneric<Integer, Integer> testGeneric = new TestGeneric<Integer, Integer>(2, 2);
-		System.out.println("Test Generic Test: " + testGeneric.compareParameters());
+		System.out.println("Test Generic: " + testGeneric.compareParameters());
 		FootBall footBall = new FootBall(LocalDate.now(), "Dhaka");
 		footBall.makeGame(footBall);
+
+		GenericDemo<String> genericDemo = new GenericDemo<String>();
+		genericDemo.set("Test");
+		System.out.println("The value is: " + genericDemo.get());
 	}
+
 }
---
+++ corejava/JavaGenerics/src/com/javaworm/generics/GenericDemo.java
@@ -0,0 +1,14 @@
+package com.javaworm.generics;
+
+public class GenericDemo<T> {
+
+	private T t;
+
+	public T get() {
+		return t;
+	}
+
+	public void set(T t) {
+		this.t = t;
+	}
+}
