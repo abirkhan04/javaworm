7d17abf81cfedcc9c3d3839b3f1698bc1735f54a
abirkhan <abir1sumon@gmail.com>
2020-07-15 12:05:55 +0600

    [] Angular animation article project is complete.

--- javascript/angular-animations-with-example/src/app/app.component.ts
+++ javascript/angular-animations-with-example/src/app/app.component.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
-import { trigger, state, style, animate, transition } from '@angular/animations';
+import {
+  trigger,
+  style,
+  animate,
+  transition,
+  query,
+  stagger,
+  keyframes,
+  state
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   animations: [
-    trigger('myInsertRemoveTrigger', [
+    trigger('ThrowACard', [state('onHand', style(
+      {
+        position: 'relative',
+        top: 0,
+        left: '500px',
+        background: '#E0E0E0',
+      })
+    ),
+    state('thrown', style(
+      {
+        position: 'relative',
+        top: '300px',
+        left: '800px',
+        background: '#004E41',
+        color: 'white',
+        opacity: 1,
+      }
+    )),
+    transition('onHand => thrown', [style({opacity: 0}), animate(1000)]),
+    transition('thrown => onHand', animate(1000)),
+    ],
+    ),
+    trigger('InsertRemoveTrigger', [
       transition(':enter', [
         style({ opacity: 0 }),
         animate('500ms', style({ opacity: 1 })),
@@ -15,12 +46,43 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
         animate('500ms', style({ opacity: 0 }))
       ])
     ]),
-  ],
+    trigger('CardStagger',
+      [transition('void => *', [
+        query(':enter', style({ opacity: 0 }), { optional: true }),
+        query(':enter', [stagger('300ms', animate('0.5s ease-in', keyframes([
+          style({ opacity: 0, transform: 'translateX(-50%)', offset: 0 }),
+          style({ opacity: 0.5, transform: 'translateX(-10px) scale(1.3)', offset: 0.3 }),
+          style({ opacity: 1, transform: 'translateX(0)', offset: 1 }),
+        ])))
+        ], { optional: true }),
+      ]),
+      transition('* => void', [
+        query(':leave', animate('2s ease-out', keyframes([
+          style({ opacity: 0.8 }),
+          style({ opacity: 0.4 }),
+          style({ opacity: 0 })
+        ])))
+      ]),
+      ],
+    )],
 })
 export class AppComponent {
-  public isShown = false;
+  public isShown: boolean = false;
+  public cardVisible: boolean = true;
+  public isThrown: boolean = false;
+  public cardLength: number = 2;
+  public cards: number[] = Array(4).fill(0).reduce(arr => { arr.push(arr.length); return arr; }, []);
 
-  public toggle() {
+  public toggleBox(): void {
     this.isShown = !this.isShown;
   }
+
+  public toggleCards(): void {
+    this.cardVisible = !this.cardVisible;
+  }
+
+  public throwACard(): void {
+    this.isThrown = !this.isThrown;
+  }
+
 }
